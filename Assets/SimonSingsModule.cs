b732917df21dc58d2ed375eb03730098124643b5
Timwi <github@timwi.de>
2018-05-20 03:00:16 +0200

    First commit.

---
+++ Assets/SimonSingsModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimonSings;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Simon Sings
+/// Created by Timwi
+/// </summary>
+public class SimonSingsModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
