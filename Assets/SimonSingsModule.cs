98a44d3c29a39c8c3239892fc485e0a24d543948
Timwi <github@timwi.de>
2018-05-22 22:52:14 +0200

    First playable version.

--- Assets/SimonSingsModule.cs
+++ Assets/SimonSingsModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using SimonSings;
 using UnityEngine;
@@ -25,29 +27,101 @@ public class SimonSingsModule : MonoBehaviour
     private int _moduleId;
     private int _curStage;
     private int _subprogress;
-    private int[] _keysToPress;
+    private List<int> _keysToPress;
     private int[] _flashingColors;
+    private int _firstNumber;
+    private int _secondNumber;
+    private bool _hasVowel;
     private bool _isSolved;
+    private Color[] _keyColors;
 
     private static readonly string[] _keyNames = @"C,C#,D,D#,E,F,F#,G,G#,A,A#,B".Split(',');
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
-        _keysToPress = new int[6];
-        for (int i = 0; i < 6; i++)
-            _keysToPress[i] = Rnd.Range(0, 12);
+        _keyColors = new Color[12];
 
-        var hasVowel = Bomb.GetSerialNumberLetters().Any(ch => "AEIOU".Contains(ch));
-        Debug.LogFormat(@"[Simon Sings #{0}] Serial number {1} a vowel, so start on the {2}.", _moduleId, hasVowel ? "contains" : "does not contain", hasVowel ? "left" : "right");
-        for (int i = 0; i < 3; i++)
-            _keysToPress[2 * i + (hasVowel ? 1 : 0)] += 12;
+        // White keys
+        assignKeyColors(new[] { 0, 2, 4, 5, 7, 9, 11 }, .9f, 1f, new float[] { 35, 288, 234, 339, 85, 131, 185 });
+        // Black keys
+        assignKeyColors(new[] { 1, 3, 6, 8, 10 }, .4f, .5f, new float[] { 359, 46, 120, 311, 175 });
 
         for (int i = 0; i < Keys.Length; i++)
             Keys[i].OnInteract = getKeyPressHandler(i);
 
+        _hasVowel = Bomb.GetSerialNumberLetters().Any(ch => "AEIOU".Contains(ch));
+        Debug.LogFormat(@"[Simon Sings #{0}] Serial number {1} a vowel, so start on the {2}.", _moduleId, _hasVowel ? "contains" : "does not contain", _hasVowel ? "left" : "right");
+
+        _keysToPress = new List<int>();
         initStage(0);
         StartCoroutine(flashing());
+    }
+
+    private void assignKeyColors(int[] keyIndices, float minLightness, float maxLightness, float[] defaultHues)
+    {
+        var numTotalTries = 0;
+        var hues = new List<float>();
+
+        tryEverythingAgain:
+        numTotalTries++;
+        hues.Clear();
+        if (numTotalTries > 1000)
+        {
+            // If we can’t find a color combination after 1000 attempts,
+            // give up and use the defaults. This should happen very rarely.
+            for (int i = 0; i < keyIndices.Length; i++)
+            {
+                hues.AddRange(defaultHues);
+                goto done;
+            }
+        }
+
+        for (int i = 0; i < keyIndices.Length; i++)
+        {
+            var numTries = 0;
+
+            tryAgain:
+            numTries++;
+            if (numTries > 10)
+                goto tryEverythingAgain;
+            var hue = Rnd.Range(0f, 360f);
+
+            for (int j = 0; j < hues.Count; j++)
+            {
+                var ds = sphericalDistance(hue, 10, hues[j], 10);
+                if (ds < .8f)
+                    goto tryAgain;
+            }
+
+            hues.Add(hue);
+        }
+
+        done:
+        for (int i = 0; i < keyIndices.Length; i++)
+        {
+            Debug.LogFormat(@"[Simon Sings #{0}] Hue for {1} is {2:0}.", _moduleId, _keyNames[keyIndices[i]], hues[i]);
+            var color = Color.HSVToRGB(hues[i] / 360, Rnd.Range(.6f, .8f), Rnd.Range(minLightness, maxLightness));
+            Keys[keyIndices[i]].GetComponent<MeshRenderer>().material.color = color;
+            Keys[keyIndices[i] + 12].GetComponent<MeshRenderer>().material.color = color;
+            _keyColors[keyIndices[i]] = color;
+        }
+    }
+
+    private static double sphericalDistance(float long1, float lat1, float long2, float lat2)
+    {
+        var dl = Mathf.Abs(lat1 - lat2);
+        var c1 = Mathf.Cos(long1 * Mathf.PI / 180);
+        var c2 = Mathf.Cos(long2 * Mathf.PI / 180);
+        var s1 = Mathf.Sin(long1 * Mathf.PI / 180);
+        var s2 = Mathf.Sin(long2 * Mathf.PI / 180);
+        var ds = Mathf.Atan2(Mathf.Sqrt(sqr(c2 * Mathf.Sin(dl)) + sqr(c1 * s2 - s1 * c2 * Mathf.Cos(dl))), s1 * s2 + c1 * c2 * Mathf.Cos(dl));
+        return ds;
+    }
+
+    private static float sqr(float value)
+    {
+        return value * value;
     }
 
     private KMSelectable.OnInteractHandler getKeyPressHandler(int i)
@@ -94,10 +168,77 @@ public class SimonSingsModule : MonoBehaviour
         }
         else
         {
-            _subprogress = 0;
-            _flashingColors = Enumerable.Range(0, 8).Select(i => ((_keysToPress[2 * stage + i / 4] % 12) & (1 << (3 - i % 4))) == 0 ? 0 : 1).ToArray();
+            var prevFlashingColors = _flashingColors;
+            var prevFirst = _firstNumber;
+            var prevSecond = _secondNumber;
 
-            Debug.LogFormat(@"[Simon Sings #{0}] Stage {1} flashing colors: {2}", _moduleId, stage + 1, _flashingColors.JoinString(", "));
+            var keys = Enumerable.Range(0, 12).ToList().Shuffle();
+            _flashingColors = keys.Take(8).ToArray();
+
+            var bits = new List<bool>();
+            for (int i = 0; i < _flashingColors.Length; i++)
+            {
+                switch (_flashingColors[i])
+                {
+                    case 0: // C
+                        bits.Add(i == 0 || i == 3 || i == 4 || i == 7);
+                        break;
+                    case 1: // C#
+                        bits.Add(i == 1 || i == 2 || i == 5 || i == 6);
+                        break;
+                    case 2: // D
+                        bits.Add(i == 0 ? Bomb.GetSerialNumberNumbers().Last() % 2 != 0 : !bits.Last());
+                        break;
+                    case 3: // D#
+                        bits.Add(i % 4 == Bomb.GetPortPlateCount() - 1);
+                        break;
+                    case 4: // E
+                        bits.Add(Bomb.GetPortPlateCount() == 0 ? Bomb.GetBatteryCount() % 2 != 0 : i % 4 == Bomb.GetPortPlates().Max(pp => pp.Length) - 1);
+                        break;
+                    case 5: // F
+                        bits.Add(_curStage == 2);
+                        break;
+                    case 6: // F#
+                        bits.Add(_curStage == Bomb.GetSerialNumberLetters().Count() - 2);
+                        break;
+                    case 7: // G
+                        bits.Add(i == 0 || i == 4 ? Bomb.GetIndicators().Count() % 2 != 0 : new[] { 1, 3, 6, 8, 10 }.Contains(_flashingColors[4 * (i / 4)]));
+                        break;
+                    case 8: // G#
+                        bits.Add(_curStage == 0 ? Bomb.GetPortCount() % 2 != 0 : new[] { 1, 3, 6, 8, 10 }.Any(key => prevFlashingColors.Contains(key)));
+                        break;
+                    case 9: // A
+                        bits.Add(_curStage == 0 ? Bomb.GetOnIndicators().Count() % 2 == Bomb.GetOffIndicators().Count() % 2 : prevFirst < 2 || prevSecond < 2);
+                        break;
+                    case 10:    // A#
+                        bits.Add(Enumerable.Range(4 * (i / 4), 4).Any(n => n % 4 != i % 4 && (_flashingColors[n] == 5 || _flashingColors[n] == 6)));
+                        break;
+                    case 11:    // B
+                        bits.Add(false);
+                        break;
+                }
+            }
+
+            for (int i = 0; i < _flashingColors.Length; i++)
+            {
+                if (_flashingColors[i] == 11)
+                {
+                    var curNumber = bits.Skip(4 * (i / 4)).Take(4).Aggregate(0, (p, n) => (p << 1) | (n ? 1 : 0));
+                    var newNumber = curNumber | (1 << (3 - i % 4));
+                    if (new[] { 2, 3, 5, 7, 11, 13 }.Contains(newNumber))
+                        bits[i] = true;
+                }
+            }
+
+            _firstNumber = bits.Take(4).Aggregate(0, (p, n) => (p << 1) | (n ? 1 : 0));
+            _secondNumber = bits.Skip(4).Take(4).Aggregate(0, (p, n) => (p << 1) | (n ? 1 : 0));
+
+            _keysToPress.Add((_firstNumber < 12 ? _firstNumber : _flashingColors[_firstNumber - 12]) + (_hasVowel ? 0 : 12));
+            _keysToPress.Add((_secondNumber < 12 ? _secondNumber : _flashingColors[_secondNumber - 12 + 4]) + (_hasVowel ? 12 : 0));
+
+            _subprogress = 0;
+
+            Debug.LogFormat(@"[Simon Sings #{0}] Stage {1} flashing colors correspond to keys: {2}", _moduleId, stage + 1, _flashingColors.Select(col => _keyNames[col]).JoinString(", "));
             Debug.LogFormat(@"[Simon Sings #{0}] Stage {1} solution: {2}", _moduleId, stage + 1, Enumerable.Range(0, 2 * (stage + 1)).Select(k => keyName(_keysToPress[k])).JoinString(", "));
         }
     }
@@ -108,13 +249,12 @@ public class SimonSingsModule : MonoBehaviour
         {
             for (int i = 0; i < _flashingColors.Length; i++)
             {
-                CentralLed.material.color = new Color(0, 0, _flashingColors[i]);
+                CentralLed.material.color = _keyColors[_flashingColors[i]];
                 yield return new WaitForSeconds(.7f);
-                CentralLed.material.color = new Color(1, 1, 1);
+                CentralLed.material.color = new Color(0x27 / 255f, 0x22 / 255f, 0x1e / 255f);
                 yield return new WaitForSeconds(.1f);
             }
 
-            CentralLed.material.color = new Color(1, 1, 1);
             yield return new WaitForSeconds(1.2f);
         }
     }
